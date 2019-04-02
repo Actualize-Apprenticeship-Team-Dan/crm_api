5e3d15a925bb268cc416ff21df5d900a861f445c
thomaszampa <thomas.zampa@gmail.com>
2019-04-01 19:23:52 -0500

    Call converter notes added to index lead page

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -38,15 +38,15 @@ class LeadsController < ApplicationController
 
     # We grab the entire text history from the Twilio API
     client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
-    messages_from_lead = client.account.messages.list({
-                  :to   => ENV['TWILIO_PHONE_NUMBER'], 
-                  :from => @lead.phone
-    })
-    messages_from_call_converter = client.account.messages.list({
-                  :to   => @lead.phone,
-                  :from => ENV['TWILIO_PHONE_NUMBER']
-    })
-    @messages = (messages_from_lead + messages_from_call_converter).sort_by {|m| m.date_sent}
+    # # messages_from_lead = client.account.messages.list({
+    #               :to   => ENV['TWILIO_PHONE_NUMBER'], 
+    #               :from => @lead.phone
+    # })
+    # messages_from_call_converter = client.account.messages.list({
+    #               :to   => @lead.phone,
+    #               :from => ENV['TWILIO_PHONE_NUMBER']
+    # })
+    # @messages = (messages_from_lead + messages_from_call_converter).sort_by {|m| m.date_sent} 
   end
 
   def update
